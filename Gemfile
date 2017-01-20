73ea418291acace850659d67adfc3b58c450b579
Pamela Renee Kenyon <pkenyon@Pamelas-MacBook-Pro.loca>
2017-01-20 11:14:52 -0500

    updating Gemfile; adding Rspec test

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org' do
   gem 'json'
   gem 'sinatra'
   gem 'rspec'
+  gem 'rack-test'
 end
---
+++ spec/helloworld_spec.rb
@@ -0,0 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../hello_world.rb'
+require 'rspec'
+require 'rack/test'
+
+describe 'The HelloWorld App' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
+    get '/hello-world'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Hello World')
+  end
+end
