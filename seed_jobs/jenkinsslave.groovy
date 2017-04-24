a03df0868fd992d9ea72f2c47ffc18ab17428fdb
Pamela Renee Kenyon <pkenyon@Pamelas-MacBook-Pro.loca>
2017-04-24 16:19:20 -0400

    adding jenkinsslave groovy file

---
+++ seed_jobs/jenkinsslave.groovy
@@ -0,0 +1,29 @@
+job('Create Ubuntu Slave') {
+  scm {
+    git {
+      remote {
+        name('origin')
+        url(gitUrl)
+      }
+      branches('master')
+      extensions {}
+    }
+  }
+    steps {
+        configure { node ->
+            // node represents <project>
+            node << builders {
+            'com.nirima.jenkins.plugins.docker.builder.DockerBuilderPublisher' {
+                    dockerFileDirectory('docker/slave')
+                    tags(class: 'java.util.Collections$UnmodifiableRandomAccessList', 'resolves-to':'java.util.Collections$UnmodifiableList') {
+                        c(class: 'list') {
+                            string('jenkinsslave')
+                        }
+                        list(reference: 'c')
+                    }
+                    cloud('Kenyonator_cloud')
+                }
+            }
+        }
+    }
+}
